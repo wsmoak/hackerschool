dc6696ec1b07ab63f7886cad621ce56088e63df0
Wendy Smoak <wsmoak@gmail.com>
2014-10-07 18:26:12 -0400

    Display the board showing the row and column plus the array index

--- connect-four.rb
+++ connect-four.rb
@@ -9,9 +9,10 @@ cols = 7
 board = Array.new(rows*cols)
 
 #display the board, visualizing single array as rows and columns
-(0..rows-1).each { |row|
-  (0..cols-1).each { |col|
-    print row.to_s + " " + col.to_s + "   "
+(0...rows).each { |row|
+  (0...cols).each { |col|
+    print row.to_s + " " + col.to_s + " "
+    print "("  +  (cols*row + col).to_s + ")   " # index into board array
   }
   puts
 }
