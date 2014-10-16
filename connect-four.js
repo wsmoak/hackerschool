7deab977a2fc0dc2ba6a83d138b8ab2b1f150830
Wendy Smoak <wsmoak@gmail.com>
2014-10-16 14:14:33 -0400

    Set up the canvas for the game

--- connect-four.js
+++ connect-four.js
@@ -1,6 +1,16 @@
-(function(){
-	// see http://benalman.com/news/2010/11/immediately-invoked-function-expression/
-		
-	
-	
+(function() {
+    // see http://benalman.com/news/2010/11/immediately-invoked-function-expression/
+    var Game = function(canvasId) {
+            var canvas = document.getElementById(canvasId);
+            var screen = canvas.getContext('2d');
+            var gameSize = {
+                x: canvas.width,
+                y: canvas.height
+            };
+        };
+
+    window.onload = function() {
+        new Game("screen");
+    };
+
 })();
