731c12b08f7ae03cf4506e5a24eb2afd147ceb38
Wendy Smoak <wsmoak@gmail.com>
2014-10-16 14:38:20 -0400

    Add some links to docs, and learn to draw a red circle.

--- connect-four.js
+++ connect-four.js
@@ -1,12 +1,19 @@
 (function() {
     // see http://benalman.com/news/2010/11/immediately-invoked-function-expression/
     var Game = function(canvasId) {
+            // see http://html5doctor.com/an-introduction-to-the-canvas-2d-api/
             var canvas = document.getElementById(canvasId);
-            var screen = canvas.getContext('2d');
             var gameSize = {
                 x: canvas.width,
                 y: canvas.height
             };
+            var context = canvas.getContext('2d');
+
+            //see http://www.html5canvastutorials.com/tutorials/html5-canvas-circles/
+            context.beginPath();
+            context.arc(150, 150, 10, 2 * Math.PI, false);
+            context.fillStyle = 'red';
+            context.fill();
         };
 
     window.onload = function() {
