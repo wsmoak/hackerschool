3b97cccbf84ca9ecf30c3b16372ad2344771ad43
Wendy Smoak <wsmoak@gmail.com>
2014-10-22 19:31:03 -0400

    Initial work on checking for a win.  Currently just counts matching adjacent pieces to the left of the last move.

--- connect-four.js
+++ connect-four.js
@@ -2,9 +2,11 @@
 // Connect Four game
 (function() {
     // see http://benalman.com/news/2010/11/immediately-invoked-function-expression/
+
+    var ROWS = 6;
+    var COLS = 7;
+
     var Game = function(canvasId) {
-            var ROWS = 6;
-            var COLS = 7;
             var red = true;
 
             // see http://html5doctor.com/an-introduction-to-the-canvas-2d-api/
@@ -35,6 +37,7 @@
                         console.log("Image Data for " + x + " " + y + " is " + imageData.data[0] + " " + imageData.data[1] + " " + imageData.data[2] + " " + imageData.data[3]);
                         if (imageData.data[3] === 0) { //if transparent meaning the space is empty
                             drawCircle(context, x, y, red);
+                            checkBoard(context, x, y, red);
                             red = !red; //toggle player color true/false -> red/black
                             break;
                         } // end if
@@ -75,6 +78,30 @@
             context.arc(x, y, 10, 0, TAU);
             context.fillStyle = red ? 'red' : 'black';
             context.fill();
+        };
+
+	    var checkBoard = function(context, x, y, red) {
+		    var columnWidth = context.canvas.width / COLS;
+		    var rowHeight = context.canvas.height / ROWS;
+
+		    var count = 0;
+		    var lastMove = context.getImageData(x, y, 1, 1);
+			xpos = x;
+			ypos = y;
+		    // look to the left and count matches
+			while ( xpos > 0) {
+                xpos -= columnWidth;
+		        var imageData = context.getImageData(xpos, y, 1, 1);
+		        if (imageData.data[3] > 0 && //not transparent
+		           lastMove.data[0] === imageData.data[0] &&
+		           lastMove.data[1] === imageData.data[1] &&  //same color
+		           lastMove.data[2] === imageData.data[2] ) {
+			       count += 1;
+		        } else {
+			       break;
+		        }
+		    } // end while
+		    console.log ( "Counted lefthand matches: " + count );
         };
 
     window.onload = function() {
